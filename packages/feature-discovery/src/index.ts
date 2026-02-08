21b280058e2353804c5f37d103b6da22963efd94
Scott Hardie <scottrmhardie@gmail.com>
2026-02-07 23:11:38 -0500

    Update index.ts

--- packages/feature-discovery/src/index.ts
+++ packages/feature-discovery/src/index.ts
@@ -4,3 +4,11 @@ export {
   type DiscoveryConfig,
   type Pattern
 } from "./discovery.js";
+
+export type {
+  FeatureProposal,
+  DiscoveryContext,
+  DiscoveryResult,
+  PriorityLevel,
+  ImpactLevel
+} from "./types.js";
