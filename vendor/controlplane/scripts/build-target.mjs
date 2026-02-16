1b5d9012904c2c5498cf81fb418c01e0c746928d
Scott Hardie <scottrmhardie@gmail.com>
2026-02-15 22:43:30 -0500

    g

--- vendor/controlplane/scripts/build-target.mjs
+++ vendor/controlplane/scripts/build-target.mjs
@@ -4,6 +4,7 @@ import { spawnSync } from 'node:child_process';
 function run(command, args, options = {}) {
   const result = spawnSync(command, args, {
     stdio: 'inherit',
+    shell: true,
     ...options,
   });
 
