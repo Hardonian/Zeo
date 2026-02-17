76b8fea40e8299334a2823263589b9a977450953
Scott Hardie <scottrmhardie@gmail.com>
2026-02-16 23:23:03 -0500

    G

--- apps/web/src/test/shims/zeo-core.ts
+++ apps/web/src/test/shims/zeo-core.ts
@@ -1,6 +1,4 @@
-export { runDecision } from '../../../../../packages/core/src/engine';
-export { canonicalizeDecisionSpec } from '../../../../../packages/core/src/canonicalize';
-export { hashDecisionSpec } from '../../../../../packages/core/src/hashing';
-export { buildEvidencePacket, buildEvidencePacketMarkdown } from '../../../../../packages/core/src/packets';
-export { computeDeterministicSeed } from '../../../../../packages/core/src/rng';
-export type { RunMeta } from '../../../../../packages/core/src/packets';
+export { runDecision, canonicalizeDecisionSpec } from '@zeo/core/client';
+export { hashDecisionSpec, computeDeterministicSeed } from '@zeo/kernel';
+export { buildEvidencePacket, buildEvidencePacketMarkdown, type RunMeta } from '@zeo/core/client';
+
